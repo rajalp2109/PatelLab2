5086ef2e20a39c5d2b8921db71478a78b5fca61e
rajalp2109 <96206178+rajalp2109@users.noreply.github.com>
2022-11-26 21:51:26 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace PatelLab2
+{
+    partial class FormLab2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLab2));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnData = new System.Windows.Forms.Button();
+            this.btnPicture = new System.Windows.Forms.Button();
+            this.picJetski = new System.Windows.Forms.PictureBox();
+            this.IblTopic = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnCalculator = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picJetski)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblData);
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnHide);
+            this.groupBox2.Controls.Add(this.btnData);
+            this.groupBox2.Controls.Add(this.btnPicture);
+            this.groupBox2.Controls.Add(this.picJetski);
+            this.groupBox2.Controls.Add(this.IblTopic);
+            this.groupBox2.Location = new System.Drawing.Point(263, 13);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(443, 279);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information";
+            // 
+            // lblData
+            // 
+            this.lblData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblData.Font = new System.Drawing.Font("Arial", 10F);
+            this.lblData.Location = new System.Drawing.Point(34, 44);
+            this.lblData.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(374, 162);
+            this.lblData.TabIndex = 6;
+            this.lblData.Visible = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(337, 215);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(83, 41);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(232, 215);
+            this.btnHide.Margin = new System.Windows.Forms.Padding(4);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(83, 41);
+            this.btnHide.TabIndex = 3;
+            this.btnHide.Text = "&Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnData
+            // 
+            this.btnData.Location = new System.Drawing.Point(126, 215);
+            this.btnData.Margin = new System.Windows.Forms.Padding(4);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(83, 41);
+            this.btnData.TabIndex = 2;
+            this.btnData.Text = "&Data";
+            this.btnData.UseVisualStyleBackColor = true;
+            this.btnData.Click += new System.EventHandler(this.btnData_Click);
+            // 
+            // btnPicture
+            // 
+            this.btnPicture.Location = new System.Drawing.Point(20, 215);
+            this.btnPicture.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPicture.Name = "btnPicture";
+            this.btnPicture.Size = new System.Drawing.Size(83, 41);
+            this.btnPicture.TabIndex = 1;
+            this.btnPicture.Text = "&Picture";
+            this.btnPicture.UseVisualStyleBackColor = true;
+            this.btnPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // picJetski
+            // 
+            this.picJetski.Image = ((System.Drawing.Image)(resources.GetObject("picJetski.Image")));
+            this.picJetski.Location = new System.Drawing.Point(34, 44);
+            this.picJetski.Margin = new System.Windows.Forms.Padding(4);
+            this.picJetski.Name = "picJetski";
+            this.picJetski.Size = new System.Drawing.Size(374, 162);
+            this.picJetski.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picJetski.TabIndex = 2;
+            this.picJetski.TabStop = false;
+            this.picJetski.Visible = false;
+            this.picJetski.Click += new System.EventHandler(this.picJetski_Click);
+            // 
+            // IblTopic
+            // 
+            this.IblTopic.AutoSize = true;
+            this.IblTopic.Location = new System.Drawing.Point(141, 21);
+            this.IblTopic.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.IblTopic.Name = "IblTopic";
+            this.IblTopic.Size = new System.Drawing.Size(161, 16);
+            this.IblTopic.TabIndex = 0;
+            this.IblTopic.Text = "Jet Skiing in Nova Scotia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 110);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Multiplier";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 166);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Answer";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(84, 44);
+            this.txtNum1.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(116, 23);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(84, 103);
+            this.txtNum2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(116, 23);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // btnCalculator
+            // 
+            this.btnCalculator.Location = new System.Drawing.Point(8, 215);
+            this.btnCalculator.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCalculator.Name = "btnCalculator";
+            this.btnCalculator.Size = new System.Drawing.Size(83, 41);
+            this.btnCalculator.TabIndex = 2;
+            this.btnCalculator.Text = "&Calculator";
+            this.btnCalculator.UseVisualStyleBackColor = true;
+            this.btnCalculator.Click += new System.EventHandler(this.btnCalculator_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(114, 215);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(83, 41);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblAnswer);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnCalculator);
+            this.groupBox1.Controls.Add(this.txtNum2);
+            this.groupBox1.Controls.Add(this.txtNum1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(231, 279);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Multiplier";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Font = new System.Drawing.Font("Arial", 10F);
+            this.lblAnswer.Location = new System.Drawing.Point(81, 162);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(116, 23);
+            this.lblAnswer.TabIndex = 6;
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormLab2
+            // 
+            this.AcceptButton = this.btnPicture;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(719, 331);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormLab2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab2 by Rajal";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picJetski)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.Button btnPicture;
+        private System.Windows.Forms.PictureBox picJetski;
+        private System.Windows.Forms.Label IblTopic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnCalculator;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblData;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatelLab2
+{
+    public partial class FormLab2 : Form
+    {
+        public FormLab2()
+        {
+            InitializeComponent();
+            txtNum1.Select();
+        }
+        //Windows Form- Lab 2
+        //Name: Rajal Patel
+        //Date: 10/03/2022
+        //Time: 10:30PM
+        //Description: Calculator and Jetskiing in Nova Scotia.
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close the form
+            Close();
+        }
+
+        private void btnPicture_Click(object sender, EventArgs e)
+        {
+            //pic show and data invisible
+            picJetski.Show();
+            lblData.Visible= false;
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            //data hide from the label and pic hide
+            lblData.Visible = false;
+            picJetski.Hide();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //data clear from the Num 1, Num 2 , Answer and cursor put in Num1
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            lblAnswer.Text = "";
+        }
+
+        private void btnData_Click(object sender, EventArgs e)
+        {
+            //put data in label, data label visible and pic hide
+            lblData.Text = "My topic is Jet Skiing(seadoo)in Nova Scotia. \"\\n\\n\"Last year I did Jet Ski Halifax Harbour Waterfront, which was an amazing experience.\rIt was my first time to try sea-doo.\nIt was very fun and easier than I thought to try!\rBest jetskiing experience I've had.";
+            lblData.Visible = true;
+            picJetski.Hide();
+        }
+
+        private void picJetski_Click(object sender, EventArgs e)
+        {
+            //display message with pic name and your name in title
+            MessageBox.Show("Jet Skiing Halifax Harbour Waterfront.", "Rajal");
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            //variable declaration
+            int operand1,multiplier, answer;
+            //variable conversion
+            operand1 = Convert.ToInt32(txtNum1.Text);
+            multiplier = Convert.ToInt32(txtNum2.Text);
+            //formula
+            answer = operand1 * multiplier;
+            //variable conversion
+            lblAnswer.Text = answer.ToString();
+
+        }
+
+    }
+}
